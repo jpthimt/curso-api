df13643766db459b2ea8e041149218a4a6e5d4cd
jpthimt <joaopedro10thim@gmail.com>
2023-07-19 14:40:20 -0300

    Criando primeira API

---
+++ src/main/java/com/curso/api/CursoApiApplication.java
@@ -0,0 +1,13 @@
+package com.curso.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursoApiApplication.class, args);
+    }
+
+}
